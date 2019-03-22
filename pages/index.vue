1ed680908436f39f0ed1ae06d5df27bc9214ea4d
h-orito <16453967+h-orito@users.noreply.github.com>
2019-03-22 19:57:08 +0900

    試しにサーバ接続してみる

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
   <section class="section">
     <div class="columns is-mobile">
       <card title="Free" icon="github-circle">
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub</a>
+        Open source on
+        <a href="https://github.com/buefy/buefy">GitHub</a>
       </card>
 
       <card title="Responsive" icon="cellphone-link">
@@ -10,25 +11,40 @@
       </card>
 
       <card title="Modern" icon="alert-decagram">
-        Built with <a href="https://vuejs.org/">Vue.js</a> and
+        Built with
+        <a href="https://vuejs.org/">Vue.js</a> and
         <a href="http://bulma.io/">Bulma</a>
       </card>
 
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
+      <card title="Lightweight" icon="arrange-bring-to-front"
+        >No other internal dependency {{ info }}</card
+      >
     </div>
   </section>
 </template>
 
 <script>
 import Card from '~/components/Card'
+import axios from 'axios'
 
 export default {
   name: 'HomePage',
 
   components: {
     Card
+  },
+
+  data() {
+    return {
+      info: 1
+    }
+  },
+
+  async created() {
+    const self = this
+    await axios.get('http://localhost:8086/wolf4busy/').then(res => {
+      self.info = res.data.hoge
+    })
   }
 }
 </script>
