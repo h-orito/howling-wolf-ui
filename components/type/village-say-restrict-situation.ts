a943e16b07cca3b110d15fc346460b047d66079b
h-orito <16453967+h-orito@users.noreply.github.com>
2020-01-29 21:54:28 +0900

    文字数カウント

--- components/type/village-say-restrict-situation.ts
+++ components/type/village-say-restrict-situation.ts
@@ -2,7 +2,8 @@ interface VillageSayRestrictSituation {
   restricted: boolean
   max_count: number | null
   remaining_count: number | null
-  max_length: number | null
+  max_length: number
+  max_line: number
 }
 
 export default VillageSayRestrictSituation
--- components/village/action/message-input.vue
+++ components/village/action/message-input.vue
@@ -25,5 +25,46 @@ export default class MessageInput extends Vue {
 
   @Prop({ type: Object })
   private situation!: VillageSaySituation | null
+
+  @Prop({ type: Object })
+  private messageType!: string
+
+  private get counter(): string {
+    if (this.restrict == null) {
+      return `${this.lineCount}, ${this.lengthCount}`
+    } else {
+      return `${this.remainingCount}, ${this.lineCount}, ${this.lengthCount}`
+    }
+  }
+
+  private get restrict(): VillageSayRestrictSituation | null {
+    if (this.situation == null) return null
+    const messageTypeSituation = this.situation.selectable_message_type_list.find(
+      restrictSituation =>
+        restrictSituation.message_type.code === this.messageType
+    )
+    return messageTypeSituation == null ||
+      !messageTypeSituation.restrict.restricted
+      ? null
+      : messageTypeSituation.restrict
+  }
+
+  private get remainingCount(): string {
+    const max = this.restrict!.max_count!
+    const remaining = this.restrict!.remaining_count!
+    return `残り回数: ${remaining.toString()}/${max.toString()}`
+  }
+
+  private get lineCount(): string {
+    const max = this.restrict == null ? 20 : this.restrict!.max_line
+    const current = this.value.split('\n').length
+    return `行数: ${current.toString()}/${max.toString()}`
+  }
+
+  private get lengthCount(): string {
+    const max = this.restrict == null ? 400 : this.restrict!.max_length
+    const current = this.value.length - this.value.split('\n').length + 1
+    return `文字数: ${current.toString()}/${max.toString()}`
+  }
 }
 </script>
