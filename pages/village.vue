46676ea8802d52573b60aab6ab0a80c9236f0219
h-orito <16453967+h-orito@users.noreply.github.com>
2020-02-12 18:42:55 +0900

    ページ分割数を元に戻す

--- pages/village.vue
+++ pages/village.vue
@@ -114,7 +114,7 @@ export default class extends Vue {
   private debugVillage: DebugVillage | null = null
 
   // message
-  private perPageCount: number = 5
+  private perPageCount: number = 50
   private currentPageNum: number | null = 1
   // latest message timer
   private latestMessageUnixTimeMilli: number = 0
