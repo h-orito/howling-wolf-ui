8af7eede87bdf683747c976ba859b20c68785aa1
h-orito <16453967+h-orito@users.noreply.github.com>
2020-07-17 20:29:36 +0900

    キャラ画像componentを作成

---
+++ components/village/chara-image.vue
@@ -0,0 +1,31 @@
+<template>
+  <img
+    :src="imageUrl"
+    :alt="chara.chara_name.name"
+    :width="chara.display.width"
+    :height="chara.display.height"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import Chara from '~/components/type/chara'
+
+@Component({
+  components: {}
+})
+export default class CharaSelectModal extends Vue {
+  @Prop({ type: Object })
+  private chara!: Chara
+
+  @Prop({ type: String, default: 'NORMAL' })
+  private faceType?: string
+
+  private get imageUrl(): string {
+    return this.chara.face_list.find(face => face.type === this.faceType)!
+      .image_url
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
