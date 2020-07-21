7f35f1cf7b9047d0bfd20abffb3f4be1d6128eb6
h-orito <16453967+h-orito@users.noreply.github.com>
2020-07-21 18:46:07 +0900

    iphone11対応

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,7 +49,7 @@ const meta = [
   {
     name: 'viewport',
     content:
-      'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no'
+      'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover'
   },
   {
     hid: 'description',
