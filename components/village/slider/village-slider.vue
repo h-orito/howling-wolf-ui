c2e3ca030ac5da1be253756d9b3cc32614b1abd2
h-orito <16453967+h-orito@users.noreply.github.com>
2020-09-14 20:15:36 +0900

    広告位置調整

--- components/village/slider/village-slider.vue
+++ components/village/slider/village-slider.vue
@@ -213,6 +213,10 @@ export default class VillageSlider extends Vue {
     line-height: 2.5rem;
     font-size: 14px;
   }
+
+  .slider-ads {
+    margin-top: 10px;
+  }
 }
 
 @media screen and (max-width: 767px) {
